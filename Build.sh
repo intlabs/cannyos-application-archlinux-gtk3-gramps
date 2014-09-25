ccc61f42cde103ad16e252e2f0970a5022cad969
intlabs <petebirley@gmail.com>
2014-09-25 02:14:03 +0100

    ...

--- Build.sh
+++ Build.sh
@@ -56,10 +56,10 @@ sudo rm -r -f "/CannyOS/build/cannyos-application-archlinux-gtk3-gramps/*"
 sudo docker stop cannyos-application-archlinux-gtk3-gramps
 
 # Launch built base container image
-sudo docker run -i -t --rm \
+sudo docker run -i -t -d \
  --privileged=true --lxc-conf="native.cgroup.devices.allow = c 10:229 rwm" \
  --volume "/CannyOS/build/cannyos-application-archlinux-gtk3-gramps":"/CannyOS/Host" \
  --name "cannyos-application-archlinux-gtk3-gramps" \
  --user "root" \
- -p 80:80 \
+ -p 80 \
  intlabs/cannyos-application-archlinux-gtk3-gramps 
